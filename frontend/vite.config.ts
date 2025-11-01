728043c7830aa8393dbe818b478962cdd81f7c39
itsDaiton <david.poslusny@email.cz>
2025-11-01 14:54:32 +0100

    chore: add dependecies and stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: "build",
+    copyPublicDir: false,
+  },
+  resolve: {
+    alias: {
+      "@frontend": resolve(__dirname, "src"),
+    },
+  },
+});
