f4aae96df479616caf87b048f40359e2ea213239
itsDaiton <david.poslusny@email.cz>
2025-11-14 18:05:07 +0100

    fix: update start script to use server.ts and set PORT from environment variable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
