ce9f9b17af0da1035b151f3fe2ca8da0683d5304
itsDaiton <david.poslusny@email.cz>
2025-11-15 16:31:06 +0100

    fix(format): resolve missing format errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,18 @@
-import type { Request, Response } from "express";
-import express from "express";
-import cors from "cors";
+import type { Request, Response } from 'express';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "*",
+    origin: '*',
   }),
 );
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
 });
 
 app.listen(PORT, () => {
