7ec677384bfc59161a822325ea6e523d29dbf852
David Poslušný <72783924+itsDaiton@users.noreply.github.com>
2025-11-18 20:04:24 +0100

    fix(services): update object destructuring in api (#12)

--- backend/src/services/message.service.ts
+++ backend/src/services/message.service.ts
@@ -3,7 +3,7 @@ import type { CreateMessageDTO } from '@backend/types/message.types';
 
 export const messageService = {
   createMessage(data: CreateMessageDTO) {
-    return messageRepository.create(data);
+    return messageRepository.create({ ...data });
   },
   getMessage(messageId: string) {
     return messageRepository.findById({ messageId });
