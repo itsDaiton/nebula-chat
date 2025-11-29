c50b7c817dc6f09314d2f755674dfb5286c8691b
itsDaiton <david.poslusny@email.cz>
2025-11-29 22:55:43 +0100

    refactor(prettier): fix format issues

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,7 +1,7 @@
-import 'dotenv/config'
-import { PrismaClient } from '../prisma/generated/prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+import 'dotenv/config';
+import { PrismaClient } from '../prisma/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 
-export const prisma = new PrismaClient({ adapter })
+export const prisma = new PrismaClient({ adapter });
