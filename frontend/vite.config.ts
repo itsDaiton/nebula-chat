a1e934a52f89df86d01950595596ad9f1fed3101
itsDaiton <david.poslusny@email.cz>
2026-03-14 17:14:47 +0100

    fix: remove vite-tsconfig-paths dependency and update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   build: {
     outDir: 'build',
     copyPublicDir: true,
