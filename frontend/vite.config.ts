8e217943a4f4e03750c6d1c4e777bd5a7f7a653e
itsDaiton <david.poslusny@email.cz>
2026-03-22 10:23:11 +0100

    feat(version): update vite.config.ts to define __dirname for versioning plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,11 @@
 import { writeFileSync } from 'node:fs';
 import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import pkg from './package.json';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const versionPlugin = () => ({
   name: 'version-json',
